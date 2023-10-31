ecbdb0d0313126b65655af57e82a10d3d14f23ee
Kimobenoit <149464386+Kimobenoit@users.noreply.github.com>
2023-10-31 10:57:57 +0200

    Add files via upload

---
+++ test git hub/test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome Page</title>
+</head>
+<body>
+    <h1>Welcome to Our Website</h1>
+
+    <?php
+    // PHP code to display a welcome message
+    $username = "John"; // You can replace this with the user's name
+    echo "<p>Hello, $username! We're glad you're here.</p>";
+    ?>
+
+    <p>Feel free to explore our website and learn more about our services.</p>
+
+    <!-- Additional content can be added here -->
+</body>
+</html>
